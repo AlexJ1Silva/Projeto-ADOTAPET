a3d1617a1f0bd627463c118b1e3a46f7eed9f74f
Alex J. Silva <111905692+AlexJ1Silva@users.noreply.github.com>
2023-03-21 21:06:11 -0300

    v1

--- force-app/main/default/lwc/uploadPhoto/uploadPhoto.js
+++ force-app/main/default/lwc/uploadPhoto/uploadPhoto.js
@@ -1,42 +1,78 @@
-import { LightningElement, track,api } from 'lwc';
+import { LightningElement, track, api, wire } from 'lwc';
 import arquivosFotosCLS from '@salesforce/apex/UploadPhotoController.arquivosFotos';
 import deleteFotoCLS from '@salesforce/apex/UploadPhotoController.deleteFoto';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { refreshApex } from '@salesforce/apex'
+//import getListaDocumentos from '@salesforce/apex/UploadPhotoController.listaDocumentos'
 export default class Fileupload extends LightningElement {
     @api recordId;
-    @track lstAllFiles;
+    @track lstAllFiles = [];
     @track error;
     @track fotoId;
+    @track wirelstAllFIles = [];
     get acceptedFormats() {
-        return ['.pdf','.png','.jpg'];
+        return ['.pdf', '.png', '.jpg'];
     }
- 
-   /*  handleUploadFinished(event) {
-        this.connectedCallback();
-    } */
- 
-    connectedCallback() {
-        arquivosFotosCLS({recordId:this.recordId})
-        .then(result=>{
-            this.lstAllFiles = result; 
-           
-           
-        });
-    }
+
     
-    apagar(event){
+
+
+    handleUploadFinished(event) {
+        this.showToast('Sucesso', 'Upload efetuado!!', 'success')
+        this.connectedCallback();
+    }
+
+    connectedCallback() {
+        arquivosFotosCLS({ recordId: this.recordId })
+            .then(result => {
+                this.lstAllFiles = result;
+                
+            });
+            
+    }
+
+   /*  @wire(getListaDocumentos) fotosList(result) {
+        this.wirelstAllFIles = result;
+    
+        if (result.data) {
+          this.lstAllFiles = result.data;
+          this.error = undefined;
+        } else if (result.error) {
+          this.error = result.error;
+          this.lstAllFiles = [];
+        }
+      } */
+
+
+    apagar(event) {
         let targetId = event.target.value;
-       
+
         console.log(targetId);
 
-        deleteFotoCLS({"fotoId" : targetId});
+        deleteFotoCLS({ "fotoId": targetId }).then(r=>{
+            this.connectedCallback();
+        });
+        this.showToast('Sucesso', 'Item Deletado', 'success')
         
-       /*  const apagaEvent = new CustomEvent('apagar', {
-            detail: {
-              apagar: targetId
-            }
-          })
-          this.dispatchEvent(apagaEvent); 
-           */
-          //console.log("###>>>>_apagar_<<<<<<#####");
+        //refreshApex(this.wirelstAllFIles);
+
+        /*  const apagaEvent = new CustomEvent('apagar', {
+             detail: {
+               apagar: targetId
+             }
+           })
+           this.dispatchEvent(apagaEvent); 
+            */
+        //console.log("###>>>>_apagar_<<<<<<#####");
+    }
+
+    showToast(titulo, msg, variant) {
+        const event = new ShowToastEvent({
+            title: titulo,
+            message: msg,
+            variant: variant,
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(event);
     }
 }
